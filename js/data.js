fee6d1eb129182766695e75bddf0a98fba0a7ad7
Samuel Cho <samuel.ms.cho@gmail.com>
2021-02-09 10:22:42 -0800

    Put the form's input values into a new object. #1

--- js/data.js
+++ js/data.js
@@ -11,6 +11,10 @@ var $newEntryForm = document.querySelector('.new-entry-form');
 
 function saveNewEntry(event) {
   event.preventDefault();
+  var entryInputs = {};
+  entryInputs.title = $newEntryForm.elements.title.value;
+  entryInputs.photo = $newEntryForm.elements.photo.value;
+  entryInputs.note = $newEntryForm.elements.note.value;
 }
 
 $newEntryForm.addEventListener('submit', saveNewEntry);
