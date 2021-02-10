59a0ef642ff4a2ad94b1194e386d7cf0abbc9a70
Samuel Cho <samuel.ms.cho@gmail.com>
2021-02-10 11:38:41 -0800

    Defined function only. Not used/called yet. Defined a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML. #2

--- js/main.js
+++ js/main.js
@@ -27,3 +27,55 @@ function saveNewEntry(event) {
 }
 
 $newEntryForm.addEventListener('submit', saveNewEntry);
+
+function createEntry(entry) {
+  var $ulEntries = document.querySelector('.entries-list');
+
+  var liJournalEntry = document.createElement('li');
+  liJournalEntry.setAttribute('class', 'journal-entry');
+  $ulEntries.appendChild(liJournalEntry);
+
+  var divRow = document.createElement('div');
+  divRow.setAttribute('class', 'row');
+  liJournalEntry.appendChild(divRow);
+
+  var divColumnHalf1 = document.createElement('div');
+  divColumnHalf1.setAttribute('class', 'column-half');
+  divRow.appendChild(divColumnHalf1);
+
+  var divEntryImageContainer = document.createElement('div');
+  divEntryImageContainer.setAttribute('class', 'entry-image-container');
+  divColumnHalf1.appendChild(divEntryImageContainer);
+
+  var imgEntryImage = document.createElement('img');
+  imgEntryImage.setAttribute('class', 'entry-image');
+  imgEntryImage.setAttribute('src', entry.photo);
+  imgEntryImage.setAttribute('alt', 'image for entry in code journal');
+  divEntryImageContainer.appendChild(imgEntryImage);
+
+  var divColumnHalf2 = document.createElement('div');
+  divColumnHalf2.setAttribute('class', 'column-half');
+  divRow.appendChild(divColumnHalf2);
+
+  var divEntryTextContainer = document.createElement('div');
+  divEntryTextContainer.setAttribute('class', 'entry-text-container');
+  divColumnHalf2.appendChild(divEntryTextContainer);
+
+  var divEntryTitle = document.createElement('div');
+  divEntryTitle.setAttribute('class', 'entry-title');
+  divEntryTextContainer.appendChild(divEntryTitle);
+
+  var headingThree = document.createElement('h3');
+  var entryHeading = document.createTextNode(entry.title);
+  headingThree.appendChild(entryHeading);
+  divEntryTitle.appendChild(headingThree);
+
+  var divEntryParagraph = document.createElement('div');
+  divEntryParagraph.setAttribute('class', 'entry-paragraph');
+  divEntryTextContainer.appendChild(divEntryParagraph);
+
+  var paragraphEntry = document.createElement('p');
+  var paragraphText = document.createTextNode(entry.note);
+  paragraphEntry.appendChild(paragraphText);
+  divEntryParagraph.appendChild(paragraphEntry);
+}
